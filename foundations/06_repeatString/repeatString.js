f4c3983495e78f414dc0004a4e4e0e3b6be8dfbd
Maidaseu <248063980+Maidaseu@users.noreply.github.com>
2026-01-28 23:37:11 +0000

    Completed Exercises  4-7

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,29 @@
-const repeatString = function() {
 
+
+const repeatString = function(string, num) {
+
+  // Reject invalid input early // 
+    if (num < 0) {
+        return 'ERROR';
+    }
+
+        // Start with an empty result //
+        let result = '';
+
+    // Build the repeated string // 
+    for (let i = 0; i < num; i++) {
+        result += string;
+    } 
+    return result;
 };
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
+
+// NOTES //
+
+
+
+
